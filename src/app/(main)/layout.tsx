28f1134c6a79c819d81fbdca40a63a9707e64ea1
OmarBouchoucha0 <bouchouchaomar38@gmail.com>
2025-06-26 16:53:25 +0100

    using route groups

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,40 +1,13 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import Navbar from "@/components/navbar";
-import Footer from "@/components/footer";
+import { ReactNode } from 'react';
+import Navbar from '@/components/navbar';
+import Footer from '@/components/footer';
 
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-    title: "e-commerce",
-    description: "a site for e-commerce",
-};
-
-export default function RootLayout({
-    children,
-}: Readonly<{
-    children: React.ReactNode;
-}>) {
+export default function MainLayout({ children }: { children: ReactNode }) {
     return (
-        <html lang="en">
-            <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-            >
-                <div>
-                    <Navbar />
-                    {children}
-                    <Footer />
-                </div>
-            </body>
-        </html >
+        <div className="min-h-screen flex flex-col">
+            <Navbar />
+            {children}
+            <Footer />
+        </div>
     );
 }
